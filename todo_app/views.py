74c8c985a6b0e4a6aafb51b1828d61cd7be249e8
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-08-25 21:06:24 +0000

    feat(todo_app): configure view for login

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,17 +1,37 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from todo_app.serializers import LoginSerializer
 
 
 # Create your views here.
 
-# TODO: Implement login view for users; we will create a new branch for that.
-#  Let's start with the name of the endpoint. // To login we would need data from the user to verify of they're
-#  authenticated. Which would mean we would need a Login serializer for that, a UserListSerialzier to display users.
+# TODO: LoginView is now what we've got to do. We get the incoming request data and serialize it, check if it's valid
+#  or not and do what needs to be done
 
 @api_view(['POST'])
 def login(request):
+
+    data = request.data
+    serializer = LoginSerializer(data=data)
+
+    if serializer.is_valid():
+        user = serializer.validated_data['user']
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            "message": True,
+            "user": UserListSerializer(user).data,
+            "token": token.key
+        }, status=status.HTTP_200_OK)
+
+
+
     return Response(
         {
-            "message": True
-        }
+            "message": False,
+            "errors": serializer.errors
+        },
+        status= status.HTTP_400_BAD_REQUEST
     )
