93d37f70c645af1cb757831fb0a756982fe48013
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-08-25 16:36:48 +0000

    feat(todo): set up views for test url route

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 # Create your views here.
 
+# TODO: Implement login view for users; we will create a new branch for that
 
 @api_view(['GET'])
 def test_api(request):
