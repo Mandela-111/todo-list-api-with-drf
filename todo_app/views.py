340858d1a28190341f7db49fe85d7e7921c5ab7a
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-08-25 16:50:40 +0000

    feat(todo_app): TODO to setup login feature

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,10 +4,12 @@ from rest_framework.response import Response
 
 # Create your views here.
 
-# TODO: Implement login view for users; we will create a new branch for that
+# TODO: Implement login view for users; we will create a new branch for that.
+#  Let's start with the name of the endpoint. // To login we would need data from the user to verify of they're
+#  authenticated. Which would mean we would need a Login serializer for that, a UserListSerialzier to display users.
 
-@api_view(['GET'])
-def test_api(request):
+@api_view(['POST'])
+def login(request):
     return Response(
         {
             "message": True
