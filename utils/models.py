77d3b186fbf8481962c88fe8f473e591a0610fd7
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-08-29 11:08:13 +0000

    feat(utils): implement an abstract timestamp model for inheritance by business models

---
+++ utils/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TimeStampedModel(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updateAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
