7dc0f33ce1c93d013bcd655fb57b73b836f710a9
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-08-31 14:25:54 +0000

    feat(todo): return name of Task object

--- todo_app/models.py
+++ todo_app/models.py
@@ -7,6 +7,7 @@ from utils.models import TimeStampedModel
 # Create your models here.
 class Tasks(TimeStampedModel):
     title, description, isCompleted = models.CharField(default='Title', max_length=50), models.CharField(), models.BooleanField()
-    createdBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
 
 
+    def __str__(self) -> str: return self.title
