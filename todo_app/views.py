579245c9e58a0bbeed5642f88395b20f6e76e6c9
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-09-24 16:31:34 +0000

    feat(todo_app): include put and delete for task/:id endpoint

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,18 +1,27 @@
 from django.db.utils import IntegrityError
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
-
+from rest_framework.authentication import TokenAuthentication
 from todo_app.serializers import LoginSerializer, RegisterSerializer, UserListSerializer
+from todo_app.models import Tasks
+from todo_app.serializers import TasksSerializer
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 
 # TODO: LoginView is now what we've got to do. We get the incoming request data and serialize it, check if it's valid
-#  or not and do what needs to be done
+#  or not and do what needs to be done.
+#  Step 2: We move on to the serializers, make sure we inherit, timestamps. It's bad practice to for a check authorization
+#  with data from the client side. We should stick to auth_tokens and JWTs. Since we want each user to own their tasks,
+#  we set the owner here in the server side by 'serializer.save(user=request.user)'
+
+
 
 
 class RegisterAPI(APIView):
@@ -91,3 +100,86 @@ def login(request):
         },
         status= status.HTTP_400_BAD_REQUEST
     )
+
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+class TasksAPI(APIView):
+    def get(self, request, task_id = None):
+
+        user = request.user
+
+        if task_id is None:
+
+            tasks = Tasks.objects.filter(user=user)
+            serializer = TasksSerializer(tasks, many=True)
+
+            return Response({
+                "message": True,
+                "tasks": serializer.data,
+            }, status=status.HTTP_200_OK)
+        else:
+
+            # foreign key objects are always tied to (user) are tied to their tasks. The user has to be the same as the
+            # one that owns the task. If not, we return a 404 error.
+            task = get_object_or_404(Tasks, id=task_id, user=user)
+            serializer = TasksSerializer(task)
+
+            return Response({
+                    "message": True,
+                    "task": serializer.data,
+                }, status=status.HTTP_200_OK)
+
+
+
+
+    def post(self, request):
+        data = request.data
+
+        serializer = TasksSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({
+                "message": True,
+                "task": serializer.data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(
+            {
+                "message": False,
+                "errors": serializer.errors
+
+            },
+            status= status.HTTP_400_BAD_REQUEST
+        )
+
+    def put(self, request, task_id):
+        user = request.user
+        task = get_object_or_404(Tasks, id=task_id, user=user)
+
+        serializer = TasksSerializer(task, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Task edited successfully",
+                "task": serializer.data
+            }, status=status.HTTP_200_OK)
+
+        return Response(
+            {
+                "message": False,
+                "errors": serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, task_id):
+        user = request.user
+        task = get_object_or_404(Tasks, id=task_id, user=user)
+
+        task.delete()
+
+        return Response({
+            "message": "Task deleted successfully"
+        }, status=status.HTTP_200_OK)
