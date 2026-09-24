b0fa90374342cecd74d2fab3952a728ae6115898
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-09-24 17:07:13 +0000

    feat(todo_app): include endpoint for marking task as complete or not

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from todo_app.views import login, RegisterAPI, TasksAPI
+from todo_app.views import login, RegisterAPI, TasksAPI, is_completed
 
 
 urlpatterns = [
     path('login', login),
     path('register', RegisterAPI.as_view()),
     path('tasks', TasksAPI.as_view()),
-    path('tasks/<int:task_id>', TasksAPI.as_view())
+    path('tasks/<int:task_id>', TasksAPI.as_view()),
+    path('tasks/<int:task_id>/mark', is_completed)
 ]
