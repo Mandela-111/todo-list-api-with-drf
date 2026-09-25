1368e0e4014ea7e49d896ed464b271cef1177e78
Mandela-the-gr8 <mandelathegr8@gmail.com>
2026-09-25 16:59:54 +0000

    feat(todo_app): Add url endpoint to allow retrieval of completed tasks by a user

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todo_app.views import login, RegisterAPI, TasksAPI, is_completed
+from todo_app.views import login, RegisterAPI, TasksAPI, is_completed, completed_tasks
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register', RegisterAPI.as_view()),
     path('tasks', TasksAPI.as_view()),
     path('tasks/<int:task_id>', TasksAPI.as_view()),
-    path('tasks/<int:task_id>/mark', is_completed)
+    path('tasks/<int:task_id>/mark', is_completed),
+    path('tasks/completed', completed_tasks)
 ]
